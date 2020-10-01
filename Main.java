5c531adc033d9c9711b43a8d6ab9deee644a288c
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-01 13:38:42 +0000

    finished example question 2.01

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+  import java.util.Scanner; 
+
 /**
- *
- * @author 
+ *example program ffor nested statements 
+ * @author Zach Ramirez
  */
 public class Main {
 
@@ -9,7 +11,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a scnaer for user input  
+    Scanner input = new Scanner(System.in); 
     
+    //ask the user if the animal has feathers 
+    System.out.println("does the animal have feathers? (yes/no)"); 
+    String featherAnswer = input.nextLine(); 
+    
+    //check to see the the feather status 
+    if(featherAnswer .equals("yes")){
+    //get the feather colour
+    System.out.println("what colour are the feathers? (red/blue) "); 
+    String featherColour = input.nextLine(); 
+
+    //determine what type of bird 
+    if(featherColour .equals("blue")){
+      System.out.println("that is a blue jay");
+    } else if(featherColour .equals("red")){ 
+      System.out.println("That is a cardinal");
+    } 
+    // if the user answered no
+    } else{ 
+
+    }  
+
+
   }
 }
